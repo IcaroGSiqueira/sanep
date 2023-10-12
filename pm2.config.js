3b6c543b6829db75c33e27affff9afbfd4ca98d3
icaroGS <icaro@bemobile.tech>
2023-10-11 21:33:14 -0300

    feat: rename apps

--- pm2.config.js
+++ pm2.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'get-mqtt',
+      name: 'get-mqtt-iwater',
       script: 'get-mqtt.py',
       interpreter: 'python3',
     },
     {
-      name: 'alert-bot',
+      name: 'alert-bot-iwater',
       script: 'alert-bot.py',
       interpreter: 'python3',
     },
